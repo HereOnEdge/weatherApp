f48ecd2298b9861d7817b0b97df015c1fc355929
Reza <reza1377maleki@gmail.com>
2022-12-21 01:37:43 +0330

    add dynamic background for each weather condition , also it will make the page more responsive

--- script.js
+++ script.js
@@ -1,7 +1,61 @@
-async function getWeather(city){
-    let Api_key = '4b8ff3976c38129b270f58272253a7e5';
-    let city_name = "tehran"
-} 
+// load images when page runs
+function loadImages() {
+    let clearDay = new Image()
+    clearDay.src = './img/clear-day.jpg'
+    let cloudDay = new Image()
+    cloudDay.src = './img/cloud-day.jpg'
+    let rainy = new Image()
+    rainy.src = './img/rain-night.jpg'
+    let snowy = new Image()
+    snowy.src = './img/snow.jpg'
+    let clearNight = new Image()
+    clearNight.src = './img/clear-night.jpg'
+    let cloudNight = new Image()
+    cloudNight.src = './img/cloud-night.jpg'
+    const images = {
+        day: {
+            clear: clearDay.src,
+            cloud: cloudDay.src,
+            rain: rainy.src,
+            snow: snowy.src,
+        },
+        night: {
+            clear: clearNight.src,
+            cloud: cloudNight.src,
+            rain: rainy.src,
+            snow: snowy.src,
+        }
+    }
+    return images
+}
+const images = loadImages()
+// show background for each weather condition
+function showBackground(locationTime, weather) {
+    let body = document.querySelector('body')
+    // check if it,s day or night
+    let isNight = false
+    if(locationTime >= 18 || locationTime <= 5) {
+        isNight = true
+    }
+    console.log(isNight)
+    // change font colors on different backgrounds
+    if (isNight === true || weather === 'Rain' || weather === 'Snow') {
+        body.style.color = '#dddddd'
+    } else {
+        body.style.color = '#222222'
+    }
+    if(isNight === false) {
+        return weather === 'Clear' ? images.day.clear : 
+        weather === 'Clouds'? images.day.cloud :
+        weather === 'Rain' ? images.day.rain:
+        weather === 'Snow' ? images.day.snow: images.day.clear
+    } else if (isNight === true) {
+        return weather === 'Clear' ? images.night.clear : 
+        weather === 'Clouds'? images.night.cloud :
+        weather === 'Rain' ? images.night.rain:
+        weather === 'Snow' ? images.night.snow: images.night.clear
+    }
+}
 // get data from input and fetch the data
 async function getLocation(){
     let firstRun = false;
@@ -53,6 +107,10 @@ async function showResults() {
     const errorBox = document.querySelector('.error-message')
     const searchBox = document.querySelector('.search')
     const bodyBox = document.querySelector('body')
+    let timeZone
+    let UTC_time
+    let locationTime
+    let timeNow = new Date()
     let weatherResult = await getLocation()
     if (weatherResult === '404') {
         searchBox.classList.add('focus')
@@ -79,6 +137,14 @@ async function showResults() {
         feelsLikeBox.textContent = Math.floor(weatherResult.main.feels_like)
         windBox.textContent = `${Math.floor((weatherResult.wind.speed))}MPH`
         humidityBox.textContent = `${weatherResult.main.humidity}%`
+        timeZone = ((weatherResult.timezone / 60) / 60)
+        UTC_time = timeNow.getUTCHours()
+        locationTime = UTC_time + timeZone
+        bodyBox.style.backgroundImage = `url(${showBackground(locationTime, weatherResult.weather[0].main)})`
+        console.log(bodyBox)
+        console.log(showBackground(locationTime, weatherResult.weather[0].main))
+        console.log(timeZone)
+        console.log(UTC_time)
         return showResults()
     }
 }
